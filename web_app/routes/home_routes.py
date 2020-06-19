019c59853fb3558b3d6385b9e3ecde3d9f9647f9
wonseoke <wonseoke@gmail.com>
2020-06-18 21:01:20 -0400

    New User Form

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, flash
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -16,7 +16,23 @@ def about():
     #return "About Me (TODO)"
     return render_template("about.html")
 
-@home_routes.route("/register")
+@home_routes.route("/users/new")
 def register():
     print("VISITED THE REGISTRATION PAGE")
-    return "Sign Up for our Product! (TODO)" 
+    return render_template("new_user_form.html")
+
+@home_routes.route("/users/create", methods=["POST"])
+def create_user():
+    print("FORM DATA:", dict(request.form))
+    # FYI: we are able to access the form data via the "request" object we import from flask
+    # ... these keys correspond with the "name" attributes of each <input> element in the form!
+    #> {'full_name': 'Example User', 'email_address': 'me@example.com', 'country': 'US'}
+
+    user = dict(request.form)
+    # todo: store in a database or google sheet!
+
+    # FYI: "warning", "primary", "danger", "success", etc. are bootstrap color classes
+    # ... see https://getbootstrap.com/docs/4.3/components/alerts/
+    # ... and the flash messaging section of the "bootstrap_layout.html" file for more details
+    flash(f"User '{user['full_name']}' created successfully! (TODO)", "warning")
+    return redirect("/")
