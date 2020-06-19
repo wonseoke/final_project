6ed871c294ee9711e0e951baf2e80d9710c9d4ba
wonseoke <wonseoke@gmail.com>
2020-06-18 20:35:23 -0400

    Shared Layouts

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -16,7 +16,7 @@ def about():
     #return "About Me (TODO)"
     return render_template("about.html")
 
-@home_routes.route("/users/new")
+@home_routes.route("/register")
 def register():
     print("VISITED THE REGISTRATION PAGE")
     return "Sign Up for our Product! (TODO)" 
